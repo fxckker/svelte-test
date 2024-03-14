c4287ebf23ee942fb9a6878c08d4885221ddcf05
Maksim <pyat06@inbox.ru>
2024-03-14 19:07:27 +0300

    add blog

---
+++ src/routes/blog/posts/1/+page.svelte
@@ -0,0 +1,16 @@
+<h1>My First Blog Post</h1>
+<p>p.lorem200</p>
+<p>p.lorem200</p>
+
+<div class="container">
+    <a href="/blog">Домой</a>
+    <slot/>
+</div>
+
+<style>
+    .container{
+        margin: 50px auto;
+        max-width: 800px;
+        padding: 0 20px;
+    }
+</style>
