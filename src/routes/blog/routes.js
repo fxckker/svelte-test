920c40f11d0db067a73b2ec3cbe520b10817a30d
Maksim <pyat06@inbox.ru>
2024-03-14 18:45:00 +0300

    add pages

--- src/routes/blog/routes.js
+++ src/routes/blog/routes.js
@@ -4,4 +4,4 @@ export const routes = [
         title: 'My First Blog Post',
         body: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Blanditiis dignissimos dolorem ea ex maiores minima minus nisi perspiciatis sapiente voluptate',
     },
-    ];
+];
